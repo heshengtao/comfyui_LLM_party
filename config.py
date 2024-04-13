dcc8b9069539b53d5553d81f097cf534e4ac9c40
ailm32442型机器人 <hst97@qq.com>
2024-04-13 22:57:26 +0800

    Add files via upload

--- config.py
+++ config.py
@@ -2,6 +2,25 @@
 import configparser
 import os
 from langchain.embeddings import HuggingFaceBgeEmbeddings
+import subprocess
+
+# 检查模型是否已经下载
+current_dir_path = os.path.dirname(os.path.realpath(__file__))
+embeddings_dir = os.path.join(current_dir_path, "model")
+embeddings_path = os.path.join(embeddings_dir, 'bge-large-zh')
+
+# 如果模型文件夹不存在，则创建文件夹
+if not os.path.exists(embeddings_dir):
+    os.makedirs(embeddings_dir)
+
+# 如果模型不存在，则克隆GitHub仓库
+if not os.listdir(embeddings_path):
+    print("正在从GitHub克隆模型...")
+    model_git_url = "https://github.com/jsonzhuwei/bge-large-zh.git"  # 模型的GitHub仓库链接
+    subprocess.run(["git", "clone", model_git_url, embeddings_path])
+    print("模型克隆完成。")
+else:
+    print("模型已存在。")
 
 
 current_file_path = os.path.abspath(__file__)
@@ -25,7 +44,6 @@ def load_api_keys(config_file):
     return api_keys
 
 # 加载词嵌入模型
-embeddings_path =os.path.join(current_dir_path,"model", 'bge-large-zh')
 
 model_kwargs = {'device': 'cuda'}  # 如果您有GPU，可以设置为 'cuda'，否则使用 'cpu'
 encode_kwargs = {'normalize_embeddings': True}  # 设置为 True 以计算余弦相似度
