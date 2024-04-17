deccc84d376f6d47d0106842a437f5ed07147949
ailm32442型机器人 <hst97@qq.com>
2024-04-17 10:15:40 +0800

    Add files via upload

--- install.py
+++ install.py
@@ -6,13 +6,19 @@ from tqdm import tqdm  # Import the tqdm library for progress bar
 
 def download_model_with_progress(model_url, local_path):
     try:
-        response = requests.get(model_url, stream=True)
+        response = requests.get(model_url, stream=True, timeout=10)
         if response.status_code == 200:
             total_size = int(response.headers.get("content-length", 0))
             block_size = 8192  # Chunk size for updating the progress bar
             with open(local_path, "wb") as f:
-                for data in tqdm(response.iter_content(block_size), total=total_size // block_size, unit="B", unit_scale=True):
+                # 使用tqdm创建进度条
+                progress = tqdm(total=total_size, unit='B', unit_scale=True, desc='Downloading')
+                for data in response.iter_content(block_size):
                     f.write(data)
+                    # 更新进度条
+                    progress.update(len(data))
+                # 关闭进度条
+                progress.close()
             print(f"Model downloaded successfully to {local_path}")
         else:
             print(f"Failed to download the model. Status code: {response.status_code}")
@@ -29,20 +35,26 @@ if not os.path.exists(embeddings_dir):
 
 print(embeddings_path)
 # 如果模型不存在，则克隆GitHub仓库
-
 if not os.path.exists(embeddings_path):
+    print("正在从github克隆模型...")
+    model_git_url = "https://github.com/jsonzhuwei/bge-large-zh.git"
     try:
-        print("正在从modelscope克隆模型...")
-        model_git_url = "https://www.modelscope.cn/AI-ModelScope/bge-large-zh.git"
-        subprocess.run(["git", "clone", model_git_url, embeddings_path])
+        subprocess.run(["git", "clone", model_git_url, embeddings_path], timeout=300)  # 设置超时时间为300秒
+    except subprocess.TimeoutExpired:
+        print("git clone 命令执行超时。")
 
-
-        print("模型克隆完成。")
-    except subprocess.CalledProcessError as e:
-        print(f"模型克隆失败：{e}")
 else:
     print("模型已存在。")
-    
+if not os.path.exists(embeddings_path):
+    print("正在从modelscope克隆模型...")
+    model_git_url = "https://www.modelscope.cn/AI-ModelScope/bge-large-zh.git"
+    try:
+        subprocess.run(["git", "clone", model_git_url, embeddings_path], timeout=300)  # 设置超时时间为300秒
+    except subprocess.TimeoutExpired:
+        print("modelscope命令执行超时。")
+else:
+    print("模型已存在。")
 model_url = "https://www.modelscope.cn/api/v1/models/AI-ModelScope/bge-large-zh/repo?Revision=master&FilePath=pytorch_model.bin"
 custom_path = os.path.join(embeddings_dir, "bge-large-zh", "pytorch_model.bin")
-download_model_with_progress(model_url, custom_path)
+if not os.path.exists(custom_path):
+    download_model_with_progress(model_url, custom_path)
