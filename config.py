42ac30c6d6a8a735cc8878d11eee6e6487ea68a4
ailm32442型机器人 <hst97@qq.com>
2024-04-29 01:02:25 +0800

    Add files via upload

--- config.py
+++ config.py
@@ -1,14 +1,6 @@
 # 获取当前文件的绝对路径
 import configparser
 import os
-from langchain.embeddings import HuggingFaceBgeEmbeddings
-import subprocess
-
-# 检查模型是否已经下载
-current_dir_path = os.path.dirname(os.path.realpath(__file__))
-embeddings_dir = os.path.join(current_dir_path, "model")
-embeddings_path = os.path.join(embeddings_dir, 'bge-large-zh')
-
 
 current_file_path = os.path.abspath(__file__)
 
@@ -30,15 +22,3 @@ def load_api_keys(config_file):
     
     return api_keys
 
-# 加载词嵌入模型
-
-model_kwargs = {'device': 'cuda'}  # 如果您有GPU，可以设置为 'cuda'，否则使用 'cpu'
-encode_kwargs = {'normalize_embeddings': True}  # 设置为 True 以计算余弦相似度
-
-bge_embeddings = HuggingFaceBgeEmbeddings(
-    model_name=embeddings_path,
-    model_kwargs=model_kwargs,
-    encode_kwargs=encode_kwargs
-)
-if __name__ == "__main__":
-    pass
