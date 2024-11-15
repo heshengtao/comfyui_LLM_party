33a2af31d2e01c43b8021bcc71125b5fee88b34a
guobalove <yanjun.wang@petalmail.com>
2024-11-15 18:59:50 +0800

    refresh auth before expired

--- custom_tool/feishu_get_history_msg.py
+++ custom_tool/feishu_get_history_msg.py
@@ -14,8 +14,9 @@ def get_tenant_access_token(token_url, app_id, app_secret):
     token_data = {"app_id": app_id, "app_secret": app_secret}
     token_headers = {"Content-Type": "application/json"}
     token_response = requests.post(token_url, data=json.dumps(token_data), headers=token_headers)
+    expire_time = time.time() + token_response.json().get('expire')
     if token_response.status_code == 200:
-        return token_response.json().get("tenant_access_token")
+        return token_response.json().get("tenant_access_token"), expire_time
     else:
         print(token_response.text)
         raise Exception("Failed to get tenant_access_token")
@@ -65,9 +66,10 @@ class FeishuGetHistory:
 
     async def refresh_auth(self):
         while True:
-            self.tenant_access_token = get_tenant_access_token(self.tenant_access_token_url, self.app_id, self.app_secret)
-            print('auth token refreshed')
-            await asyncio.sleep(600)
+            if time.time() >= self.expire_time - 1800:
+                self.tenant_access_token, self.expire_time = get_tenant_access_token(self.tenant_access_token_url, self.app_id, self.app_secret)
+                print(f'auth token refreshed, expired at {self.expire_time}, now: {time.time()}, duration: {self.expire_time - time.time()}')
+            await asyncio.sleep(1)
 
     def get_history(
         self, app_id=None, app_secret=None, chat_type=None, chat_id=None, mode="auto", time_diff_sec=60  # oc_xxx
@@ -85,11 +87,12 @@ class FeishuGetHistory:
         self.receive_id_type = "chat_id"
         self.receive_id = self.chat_id
 
+        self.tenant_access_token, self.expire_time = get_tenant_access_token(self.tenant_access_token_url, self.app_id, self.app_secret)
+        print(f'auth token init, expired at {self.expire_time}, now: {time.time()}, duration: {self.expire_time - time.time()}')
+
         if not self.auth_thread_started:
             threading.Thread(target=lambda: asyncio.run(self.refresh_auth()), daemon=True).start()
             self.auth_thread_started = True
-        
-        self.tenant_access_token = get_tenant_access_token(self.tenant_access_token_url, self.app_id, self.app_secret)
 
         headers = {
             "Authorization": f"Bearer {self.tenant_access_token}",
